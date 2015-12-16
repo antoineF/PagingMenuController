3930bf595e038145c0c94a8d3093b9989118326d
Antoine <antoine.froissart@gmail.com>
2015-12-16 15:17:37 +0100

    Set moveToMenuPage public

--- Pod/Classes/PagingMenuController.swift
+++ Pod/Classes/PagingMenuController.swift
@@ -406,7 +406,7 @@ public class PagingMenuController: UIViewController, UIScrollViewDelegate {
     
     // MARK: - Page controller
     
-    private func moveToMenuPage(page: Int, animated: Bool) {
+    internal func moveToMenuPage(page: Int, animated: Bool) {
         let lastPage = currentPage
         currentPage = page
         currentViewController = pagingViewControllers[page]
